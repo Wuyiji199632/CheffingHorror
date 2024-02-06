35560e4716b7111d7d196b32cf44d0d25ab49b72
Wuyiji199632 <94507053+Wuyiji199632@users.noreply.github.com>
2024-02-06 14:56:37 +0000

    Camera works well now for the rotations

---
+++ Cheffing_Horror_Game/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    //public Transform playerBody;
+    float xRotation=0,yRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMoseRotationMovement();
+    }
+
+    private void UpdateMoseRotationMovement()
+    {
+        float mouseX=Input.GetAxis("Mouse X")*mouseSensitivity*Time.deltaTime;
+
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseX;
+        xRotation= Mathf.Clamp(xRotation, -90f, 90f);
+
+
+        yRotation -= mouseY;
+        yRotation = Mathf.Clamp(yRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(yRotation, xRotation, 0f);
+        this.transform.Rotate(Vector3.up * mouseX);
+    }
+}

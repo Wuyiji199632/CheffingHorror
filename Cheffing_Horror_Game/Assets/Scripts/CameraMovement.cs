cbb8ca1dda0c6ea5f4800f050b9fc88ebf2b307c
Wuyiji199632 <94507053+Wuyiji199632@users.noreply.github.com>
2024-02-07 15:20:47 +0000

    Push!!!

--- Cheffing_Horror_Game/Assets/Scripts/CameraMovement.cs
+++ Cheffing_Horror_Game/Assets/Scripts/CameraMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 150f;
     //public Transform playerBody;
     float xRotation=0,yRotation = 0f;
     // Start is called before the first frame update

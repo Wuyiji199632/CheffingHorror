8f947c0e9ea559e0e4cc962ea32f040de8e047a7
Wuyiji199632 <94507053+Wuyiji199632@users.noreply.github.com>
2024-02-09 14:05:32 +0000

    get the basic movement done, still needs fixing for the fact of dashing to the sky

--- Cheffing_Horror_Game/Assets/Scripts/CameraMovement.cs
+++ Cheffing_Horror_Game/Assets/Scripts/CameraMovement.cs
@@ -6,18 +6,23 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public float mouseSensitivity = 120f;
+
+    public float movementSpeed = 10f;
     //public Transform playerBody;
     float xRotation=0,yRotation = 0f;
+
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        rb= GetComponent<Rigidbody>();  
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdateMoseRotationMovement();
+        UpdateMoseRotationMovement(); UpdateTranslationMovement();
     }
 
     private void UpdateMoseRotationMovement()
@@ -27,13 +32,21 @@ public class CameraMovement : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation += mouseX;
-        xRotation= Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation= Mathf.Clamp(xRotation, -360f, 360f);
 
-
+       
         yRotation += mouseY;
-        yRotation = Mathf.Clamp(yRotation, -90f, 90f);
+        yRotation = Mathf.Clamp(yRotation, -90f,90f);
 
-        transform.localRotation = Quaternion.Euler(yRotation, xRotation, 0f);
-        this.transform.Rotate(Vector3.up * mouseX);
+
+        transform.localRotation = Quaternion.Euler(-yRotation, xRotation, 0f);
+        //this.transform.Rotate(Vector3.up * mouseX);
+    }
+
+    private void UpdateTranslationMovement()
+    {
+        float horizontalTrans = Input.GetAxis("Horizontal"),verticalTrans=Input.GetAxis("Vertical");
+
+        transform.Translate(horizontalTrans*movementSpeed, 0, verticalTrans*movementSpeed);
     }
 }
